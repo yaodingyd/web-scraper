07eecf48642d3f4d98cfc234508654759d452abf
Yao Ding <dingyaosd@gmail.com>
2017-01-22 13:14:36 -0500

    update

--- index.js
+++ index.js
@@ -1,85 +1,32 @@
-'use strict'
+var osmosis = require('osmosis');
+var fs = require('fs');
 
-const rq = require('request-promise');
-const cheerio = require('cheerio');
-const _ = require('lodash');
-const fs = require('fs');
-let $;
 let stream = fs.createWriteStream('result.txt');
 
-const BASE_URL = 'https://www.revolico.com';
-const LIMIT = 10;
-const options = {
-    uri: BASE_URL,
-    transform: function (body) {
-        return cheerio.load(body);
+osmosis
+.get('https://www.revolico.com')
+.find('.module li a:first')
+.set('category')
+.follow('@href')
+.paginate('a.pagenav[title="Siguiente"]')
+.find('.adsterix_set tr td > a:not([href^="javascript"])')
+.set('entry')
+.follow('@href')
+.set({
+    'title':        'h1.headingText',
+    'description':  '.showAdText',
+    'images':       ['.photo-frame img@src'],
+    'email':        '#contact .normalText > a@href',
+    'telephone':    '#contact .normalText:first@html'
+})
+.data(function(listing) {
+    stream.write(listing.title);
+    stream.write(listing.images.join('\n'));
+    stream.write(listing.description);
+    if (listing.email) {
+      console.log(listing.email);
     }
-}
-
-rq(options)
-    .then(function($) {
-        let search = $('li a');
-        let category = _.flatMap(search, function(item){
-            return item.attribs.href;
-        })
-        return category;
-    })
-    .then(function(category) {
-        for (let i = 0; i < LIMIT; i++) {
-            rq({
-                uri: BASE_URL + category[i],
-                transform: function (body) {
-                    return cheerio.load(body);
-                }
-            })
-            .then(function($) {
-                let search = $('tr td a');
-                let items = _.filter(search, function(item){
-                    if (item.attribs.href !== 'javascript:')
-                    return item.attribs.href;
-                }).map(function(item){
-                    return item.attribs.href;
-                })
-                return items;
-            })
-            .then(function(items)  {
-                for (let j = 0; j < LIMIT; j++){
-                    rq({
-                        uri: BASE_URL + items[j],
-                        transform: function (body) {
-                            return cheerio.load(body);
-                        }
-                    })
-                    .then(function($){
-                        let description = $('.showAdText').html();
-                        let photos = _.map($('.photo-frame img'), function(item){
-                            return item.attribs['data-cfsrc'];
-                        }).filter(function(item) {
-                            return item !== undefined;
-                        });
-                        let contactHeaders = $('#contact .headingText2');
-                        let contactContents = $('#contact .normalText');
-
-                        let contacts = _.map(contactHeaders, function(node, index) {
-                            if (contactContents.eq(index).find('a').length > 0)
-                            return contactHeaders.eq(index).html() + contactContents.eq(index).find('a').attr('href');
-                            else
-                            return contactHeaders.eq(index).html() + contactContents.eq(index).text();
-                        })
-                        console.log('==============Description===========\n' + description);
-                        console.log('================Photo==============\n' + photos.join('\n'));
-                        console.log('===============Contacts============\n' + contacts.join('\n'));
-                        // do something else here, maybe store in db    
-                        stream.write(description);
-                        stream.write(photos.join('\n'));
-                        stream.write(contacts.join('\n'));
-                            
-                    })
-                }
-            })
-        }
-    })
-    .catch(function(e){
-        console.log(e);
-    })
-
+})
+.log(console.log)
+//.error(console.log)
+//.debug(console.log)
