8885c6dec054bd96d63575ec7052086d1fffb165
yding <yding@webmd.net>
2017-01-23 16:12:29 -0500

    update

---
+++ index-log.js
@@ -0,0 +1,79 @@
+var osmosis = require('osmosis');
+var email = require('./email');
+var moment = require('moment');
+
+moment.locale('es');
+
+osmosis
+.get('https://www.revolico.com', 1)
+.find('.module li a:first')
+.set('category')
+.follow('@href')
+.paginate('a.pagenav[title="Siguiente"]')
+.find('.adsterix_set tr td > a:not([href^="javascript"])')
+.set({
+    'url':          '@href'         
+})
+.follow('@href')
+.set({
+    'title':        'h1.headingText',
+    'description':  '.showAdText',
+    'images':       ['.photo-frame img@src'],
+    'email':        '#contact .normalText > a .__cf_email__@data-cfemail'
+})
+.then(function(context, data){
+  let contacts = context.find('#lineBlock');
+  for (let i = 1; i < contacts.length; i++) {
+      let title = contacts[i].find('.headingText2')[0].innerHTML;
+      let content =  contacts[i].find('.normalText')[0].innerHTML;
+      if (title === 'Nombre: ') {
+        data['name'] = content;
+      }
+      if (/^Tel/.test(title)) {
+        data['telephone'] = content;
+      }
+      if (/^Precio/.test(title)) {
+        data['price'] = content.match(/^[\d]+$/);
+      }
+      if (/^Fec/.test(title)) {
+        data['date'] = content;
+      }
+  }
+})
+.data(function(listing) {
+    if (listing.email) {
+      listing.email = email(listing.email);
+    } else {
+      listing.email = 'place@hold.it';
+    }
+    listing.images = listing.images.join('\n');
+    if (listing.date) {
+      listing.date = moment(listing.date, 'LLLL').format('YYYY-MM-DD hh:mm:ss');
+    }
+    if (!listing.price) {
+      listing.price = '';
+    }
+    if (!listing.name) {
+      listing.name = '';
+    }
+    if (!listing.telephone) {
+      listing.telephone = '';
+    }
+    let values = [];
+    console.log('Title is : ' + listing.title);
+    console.log('Email is : ' + listing.email);
+    console.log('Phone is : ' + listing.telephone);
+    console.log('Name  is : ' + listing.name);
+    console.log('Date  is : ' + listing.date);
+    console.log('URL   is : ' + listing.url);
+    console.log('Price is : ' + listing.price);
+    console.log('Image is : ' + listing.images);
+    console.log('*************************************************************************************');
+    
+})
+.done(function(){
+  console.log('DONE!');
+})
+//.log(console.log)
+//.error(console.log)
+//.debug(console.log)
